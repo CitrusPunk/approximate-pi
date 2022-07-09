7cc27ddb72d5e0899a6914e8c8c365cb4b904cc7
Marian K <marian.kloesler@gmx.de>
2022-07-09 21:42:40 +0200

    update: Perl Version implemented

--- Perl/approximatePi.pl
+++ Perl/approximatePi.pl
@@ -1,6 +1,39 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 use strict;
 use warnings;
 
-print "hi NAME\n";
+my $iterations;
+my $total; 
+my $inCircle;
+my $radius;
+my $input;
+my $x;
+my $y;
+my $pi;
 
+$iterations = 10000;
+$total = 0.0;
+$inCircle = 0.0;
+$radius = 1.0;
+# get user input, otherwise use default value
+print "Enter number of iterations: ";
+$input = <>;
+chomp($input);
+if(int($input)){
+  $iterations = $input;
+}
+# for-loop that generates random point and checks if they are in circle or not
+my @a = (1..$iterations);
+for(@a){
+  $x = rand(1)*$radius;
+  $y = rand(1)*$radius;
+  $total++;
+  my $distance = sqrt($x**2 + $y**2);
+  if($distance < $radius){
+      $inCircle++;
+  }
+}
+# pi approximation
+$pi = 4 * ($inCircle / $total);
+# write output
+print "Approximation of pi is: ".$pi;
