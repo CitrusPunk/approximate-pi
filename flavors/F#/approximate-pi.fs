a86cf9bd8d32cdb5481114bacc705498855eba71
CitrusPunk <marian.kloesler@gmx.de>
2023-04-19 14:54:31 +0200

    added f# implementation of approximate pi

---
+++ flavors/F#/approximate-pi.fs
@@ -0,0 +1,25 @@
+// REQUIREMENTS:
+// Web Browser
+// USAGE:
+// Open https://try.fsharp.org/
+// Copy this code and paste it into the editor
+// To run it, click run in the left-upper corner
+
+open System
+
+let radius = 1.0
+let rnd = new Random()
+let randomFloat = rnd.NextDouble() * 2.0 - radius |> float
+
+let iterations = 10000000
+let mutable in_circle = 0
+for i in 1..iterations do
+    let rnd = new Random()
+    let x = rnd.NextDouble() * 2.0 - radius |> float
+    let y = rnd.NextDouble() * 2.0 - radius |> float
+    let distance = sqrt (x*x + y*y)
+    if float distance < radius then
+        in_circle <- in_circle + 1
+    
+let pi = 4.0 * float in_circle / float iterations 
+printfn "Random float value: %f" pi
